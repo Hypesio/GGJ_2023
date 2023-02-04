31ab24fb57f3ece411316a60dccf57cfa2af6ebe
Antoine A <aubin.antoine@lilo.org>
2023-02-04 10:19:06 +0100

    Finish tree logic

--- src/genealogie/PictureBehaviour.gd
+++ src/genealogie/PictureBehaviour.gd
@@ -1,26 +1,17 @@
 extends Spatial
 
+signal on_selected(event)
+
 var is_selected = false
 var previous_mouse_position = null
-var mouse_speed = 0.005
 
 func _ready():
 	var body = $PhotoTree/KinematicBody
 	body.connect("input_event", self, "on_input_event")
 
 # Call on click
-func on_input_event(camera, event, click_position, click_normal, shape_idx):
+func on_input_event(_camera, event, _click_position, _click_normal, _shape_idx):
 	if event.is_pressed()  :
-		is_selected = true
-
-func _process(delta):
-	if is_selected && Input.is_mouse_button_pressed(1): # Left click
-		var click_position = get_viewport().get_mouse_position()
-		if (previous_mouse_position != null) :
-			var direction_move = previous_mouse_position - click_position 
-			translation += Vector3(direction_move.x, direction_move.y, 0) * mouse_speed
-		previous_mouse_position = click_position
-	elif is_selected :
-		is_selected = false
+		emit_signal("on_selected", event)
 
 
